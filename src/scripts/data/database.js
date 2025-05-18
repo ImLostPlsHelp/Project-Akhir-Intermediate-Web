2795027c21d3ca577c8b6fefcd934b9fc4fea9b7
ImLostPlsHelp <107686662+ImLostPlsHelp@users.noreply.github.com>
2025-05-18 23:17:25 +0700

    made database.js

--- src/scripts/data/database.js
+++ src/scripts/data/database.js
@@ -1,1 +1,35 @@
 import { openDB } from "idb";
+
+const DATABASE_NAME = "userstories";
+const DATABASE_VERSION = 1;
+const OBJECT_STORE_NAME = "saved-stories";
+
+const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
+  upgrade: (database) => {
+    database.createObjectStore(OBJECT_STORE_NAME, {
+      keyPath: "id",
+    });
+  },
+});
+
+const Database = {
+  async getAllStories() {
+    return (await dbPromise).getAll(OBJECT_STORE_NAME);
+  },
+
+  async putStory(story) {
+    if (!id) {
+      throw new Error("`id` is required.");
+    }
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
+  },
+
+  async removeStory(id) {
+    if (!id) {
+        throw new Error("`id` is required.");
+    }
+    return (await dbPromise).delete()
+  }
+};
+
+export default Database;
